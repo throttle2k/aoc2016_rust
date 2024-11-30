f95c6b98408b1f131f9095e410f44c45b13b1819
throttle2k <lomax79@gmail.com>
2024-11-30 13:09:19 +0100

    Day 14: One-Time Pad - part 2

--- days/day14/src/main.rs
+++ days/day14/src/main.rs
@@ -6,6 +6,7 @@ fn find_match(
     input: &str,
     c: char,
     count_repeat: usize,
+    stretch: usize,
     hash_map: &mut HashMap<u32, String>,
 ) -> Option<u32> {
     let value = format!("{c}").repeat(count_repeat);
@@ -16,8 +17,10 @@ fn find_match(
             let digest = if let Some(digest) = hash_map.get(&idx) {
                 digest.clone()
             } else {
-                let digest = md5::compute(format!("{input}{}", idx));
-                let digest = format!("{:x}", digest);
+                let digest = (0..=stretch).fold(format!("{input}{}", idx), |digest, _| {
+                    let digest = md5::compute(digest);
+                    format!("{:x}", digest)
+                });
                 hash_map.insert(idx, digest.clone());
                 digest
             };
@@ -34,9 +37,20 @@ fn find_match(
     result
 }
 
-fn is_triple(n: u32, input: &str) -> Option<char> {
-    let digest = md5::compute(format!("{input}{n}"));
-    let digest = format!("{:x}", digest);
+fn is_triple(
+    n: u32,
+    input: &str,
+    stretch: usize,
+    hash_map: &mut HashMap<u32, String>,
+) -> Option<char> {
+    let digest = if let Some(digest) = hash_map.get(&n) {
+        digest.clone()
+    } else {
+        (0..=stretch).fold(format!("{input}{n}"), |digest, _| {
+            let digest = md5::compute(digest);
+            format!("{:x}", digest)
+        })
+    };
     let mut result = None;
     digest.chars().collect::<Vec<_>>().windows(3).any(|c| {
         if c[0] == c[1] && c[1] == c[2] {
@@ -49,13 +63,13 @@ fn is_triple(n: u32, input: &str) -> Option<char> {
     result
 }
 
-fn find_hashes(count: usize, input: &str) -> Vec<u32> {
+fn find_hashes(count: usize, input: &str, stretch: usize) -> Vec<u32> {
     let mut result = vec![];
     let mut idx = 0;
     let mut hash_map = HashMap::new();
     while result.len() < count {
-        if let Some(c) = is_triple(idx, input) {
-            if find_match(idx, input, c, 5, &mut hash_map).is_some() {
+        if let Some(c) = is_triple(idx, input, stretch, &mut hash_map) {
+            if find_match(idx, input, c, 5, stretch, &mut hash_map).is_some() {
                 result.push(idx);
             }
         }
@@ -65,8 +79,10 @@ fn find_hashes(count: usize, input: &str) -> Vec<u32> {
 }
 
 fn main() {
-    let hashes = find_hashes(64, "ngcjuoqr");
+    let hashes = find_hashes(64, "ngcjuoqr", 0);
     println!("Part 1 = {}", hashes.last().unwrap());
+    let hashes = find_hashes(64, "ngcjuoqr", 2016);
+    println!("Part 2 = {}", hashes.last().unwrap());
 }
 
 #[cfg(test)]
@@ -80,7 +96,7 @@ mod day14_tests {
         expected = { None, None, Some('8'), None, None, Some('e'), Some('9') }
     )]
     fn is_triple(input: u32, expected: Option<char>) {
-        assert_eq!(is_triple(input, "abc"), expected);
+        assert_eq!(is_triple(input, "abc", 0, &mut HashMap::new()), expected);
     }
 
     #[parameterized(
@@ -90,14 +106,32 @@ mod day14_tests {
     )]
     fn find_match(input: u32, c: char, expected: Option<u32>) {
         assert_eq!(
-            find_match(input, "abc", c, 5, &mut HashMap::new()),
+            find_match(input, "abc", c, 5, 0, &mut HashMap::new()),
             expected
         );
     }
 
     #[test]
     fn part1() {
-        let hashes = find_hashes(64, "abc");
+        let hashes = find_hashes(64, "abc", 0);
         assert_eq!(*hashes.last().unwrap(), 22728);
+    }
+
+    #[parameterized(
+        input = { 5, 10, 22551 },
+        c = { '2', 'e', 'f' },
+        expected = { None, Some(89), Some(22859) }
+    )]
+    fn find_match_with_stretch(input: u32, c: char, expected: Option<u32>) {
+        assert_eq!(
+            find_match(input, "abc", c, 5, 2016, &mut HashMap::new()),
+            expected
+        );
+    }
+
+    #[test]
+    fn part2() {
+        let hashes = find_hashes(64, "abc", 2016);
+        assert_eq!(*hashes.last().unwrap(), 22551);
     }
 }
