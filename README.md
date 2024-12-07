418551c0d4765b1becb1616eca3624d4d632775a
throttle2k <lomax79@gmail.com>
2024-12-07 11:51:12 +0100

    Day 20: Firewall Rules - part 2

--- README.md
+++ README.md
@@ -21,4 +21,4 @@ My solutions for Advent of Code 2016 in Rust
 - [x] Day 17: Two Steps Forward
 - [x] Day 18: Like a Rogue
 - [x] Day 19: An Elephant Named Joseph
-- [ ] Day 20: Firewall Rules
+- [x] Day 20: Firewall Rules
--- days/day20/src/main.rs
+++ days/day20/src/main.rs
@@ -1,18 +1,18 @@
 use common::read_input;
 
-#[derive(Debug)]
-struct Rule(u32, u32);
+#[derive(Debug, Clone)]
+struct Rule {
+    start: u32,
+    end: u32,
+}
 
 impl From<&str> for Rule {
     fn from(value: &str) -> Self {
         let (min, max) = value.trim().split_once('-').unwrap();
-        Self(min.parse().unwrap(), max.parse().unwrap())
-    }
-}
-
-impl Rule {
-    fn is_valid(&self, address: u32) -> bool {
-        address < self.0 || address > self.1
+        Self {
+            start: min.parse().unwrap(),
+            end: max.parse().unwrap(),
+        }
     }
 }
 
@@ -21,30 +21,53 @@ struct Rules(Vec<Rule>);
 
 impl From<&str> for Rules {
     fn from(value: &str) -> Self {
-        let rules = value.trim().lines().map(|l| l.into()).collect();
+        let mut rules = value
+            .trim()
+            .lines()
+            .map(|l| Rule::from(l))
+            .collect::<Vec<_>>();
+        rules.sort_by_key(|r| r.start);
+        let rules = rules.iter().fold(Vec::<Rule>::new(), |mut merged, r| {
+            if let Some(last) = merged.last_mut() {
+                if last.end >= r.start {
+                    last.end = last.end.max(r.end);
+                } else {
+                    merged.push(r.clone());
+                }
+            } else {
+                merged.push(r.clone());
+            }
+            merged
+        });
+
         Self(rules)
     }
 }
 
 impl Rules {
-    fn is_valid(&self, address: u32) -> bool {
-        self.0.iter().all(|rule| rule.is_valid(address))
+    fn min_valid(&self) -> u32 {
+        self.0
+            .windows(2)
+            .skip_while(|rules| rules[0].end == rules[1].start - 1)
+            .next()
+            .unwrap()[0]
+            .end
+            + 1
     }
-}
 
-fn find_min_valid(min: u32, max: u32, rules: &Rules) -> u32 {
-    (min..max)
-        .skip_while(|address| !rules.is_valid(*address))
-        .next()
-        .unwrap()
+    fn count_valid(&self) -> u32 {
+        self.0
+            .windows(2)
+            .map(|rules| rules[1].start - rules[0].end - 1)
+            .sum()
+    }
 }
 
 fn main() {
     let input = read_input("day20.txt");
-    println!(
-        "Part 1 = {}",
-        find_min_valid(0, u32::max_value(), &input.as_str().into())
-    );
+    let rules = Rules::from(input.as_str());
+    println!("Part 1 = {}", rules.min_valid());
+    println!("Part 2 = {}", rules.count_valid());
 }
 
 #[cfg(test)]
@@ -56,7 +79,7 @@ mod day20_tests {
         let input = r#"5-8
 0-2
 4-7"#;
-        let rules = input.into();
-        assert_eq!(find_min_valid(0, 9, &rules), 3);
+        let rules = Rules::from(input);
+        assert_eq!(rules.min_valid(), 3);
     }
 }
